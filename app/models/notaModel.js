72316688846f34eb30d54c6ba7992dadaf7ce7eb
Berenice <berenicearagon3@gmail.com>
2025-04-22 18:59:38 -0600

    máaaaaaaas

--- app/models/notaModel.js
+++ app/models/notaModel.js
@@ -1,22 +1,3 @@
-const connection = require('../../config/db');
-
-// Eliminar una nota
-const deleteNota = (id) => {
-  return new Promise((resolve, reject) => {
-    connection.query('DELETE FROM notas WHERE id = ?', [id], (err, results) => {
-      if (err) {
-        return reject(err);
-      }
-      resolve(results.affectedRows > 0); // Devuelve true si se eliminó al menos una fila
-    });
-  });
-};
-
-module.exports = {
-  deleteNota,
-};
-
-
 
 const connection = require('../../config/db');
 
