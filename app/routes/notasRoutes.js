c5fd7915fbf56ed4a499776ff17d9e96bdcd8f2a
Bere nice <160133610+Achucita@users.noreply.github.com>
2025-04-23 11:06:59 -0600

    Update notasRoutes.js

--- app/routes/notasRoutes.js
+++ app/routes/notasRoutes.js
@@ -1,11 +1,5 @@
 const Router = require('koa-router');
-const {
-  getAllNotasHandler,
-  getNotaByIdHandler,
-  createNotaHandler,
-  updateNotaHandler,
-  deleteNotaHandler,
-} = require('../controllers/notasController');
+const { getAllNotasHandler, createNotaHandler } = require('../controllers/notasController');
 
 // Crear una nueva instancia de Koa Router
 const router = new Router({ prefix: '/api/notas' });
@@ -13,17 +7,8 @@ const router = new Router({ prefix: '/api/notas' });
 // Ruta para obtener todas las notas
 router.get('/', getAllNotasHandler);
 
-// Ruta para obtener una nota por ID
-router.get('/:id', getNotaByIdHandler);
-
 // Ruta para crear una nueva nota
 router.post('/', createNotaHandler);
-
-// Ruta para actualizar una nota
-router.put('/:id', updateNotaHandler);
-
-// Ruta para eliminar una nota
-router.delete('/:id', deleteNotaHandler);
 
 // Exportar el router
 module.exports = router;
