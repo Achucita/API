dbb60dce358a2498a93e9d9b5b87e6e32fbefbb2
Bere nice <160133610+Achucita@users.noreply.github.com>
2025-04-23 11:08:58 -0600

    Update init.sql

--- config/init.sql
+++ config/init.sql
@@ -6,7 +6,3 @@ CREATE TABLE IF NOT EXISTS notas (
   fecha_creacion DATETIME NOT NULL,
   fecha_actualizacion DATETIME NOT NULL
 );
-
--- Insertar una nota de ejemplo (opcional)
-INSERT INTO notas (titulo, contenido, fecha_creacion, fecha_actualizacion)
-VALUES ('Nota de Ejemplo', 'Esta es una nota de prueba.', NOW(), NOW());
